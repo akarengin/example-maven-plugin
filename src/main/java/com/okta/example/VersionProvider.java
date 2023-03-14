e3ca470ae5c2daed126474d8d0fc27c597726171
akarengin <engin.akar1@gmail.com>
2023-03-14 20:21:26 +0300

    first commit

---
+++ src/main/java/com/okta/example/VersionProvider.java
@@ -0,0 +1,8 @@
+package com.okta.example;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+public interface VersionProvider {
+
+    String getVersion(String command) throws MojoExecutionException;
+}
